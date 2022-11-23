554557bab31c085d9f03565e853936875656e97b
Sampath Kumar S <sampath.s@gdn-commerce.com>
2022-11-23 22:43:17 +0530

    OP

--- src/main/java/DummyPackage/className.java
+++ src/main/java/DummyPackage/className.java
@@ -2,6 +2,6 @@ package DummyPackage;
 
 public class className {
     public static void main(String[] args) {
-        System.out.println("DUMMY CLASSES YA");
+        System.out.println("DUMMY CLASSES ");
     }
 }
